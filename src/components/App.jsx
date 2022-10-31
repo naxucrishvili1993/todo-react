3f45befe344a2b4e26ea75014ca369a4871855e8
naxucrishvili1993 <giorginaxucrishvili70@gmail.com>
2022-11-01 00:09:37 +0400

    Add ToDoItem.jsx , Changed Code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,54 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-   const [inputText, setInputText] = useState('');
-   const [items, setItems] = useState([]);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
 
-   function handleChange(event) {
-      const newValue = event.target.value;
-      setInputText(newValue);
-   }
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
 
-   function addItem() {
-      if(inputText !== '')
-         setItems((previousItems) => [...previousItems, inputText]);
-      setInputText('');
-   }
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-   return (
-      <div className="container">
-         <div className="heading">
-            <h1>To-Do List</h1>
-         </div>
-         <div className="form">
-            <input onChange={handleChange} type="text" value={inputText} />
-            <button onClick={addItem}>
-               <span>Add</span>
-            </button>
-         </div>
-         <div>
-            <ul>
-               {items.map(item => <li>{item}</li>)}
-            </ul>
-         </div>
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
       </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              itemText={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
